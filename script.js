12229e44ff4bd94a41fdaf95ae8d2196fe13686f
Zhong Dian <ceo@myaiol.com>
2016-12-22 18:26:11 +0800

    Roughly Done, without AJAX

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function pageOnload(){
+
+}
+
+function draw() {
+  document.getElementById("formPage").style.display = "none";
+  document.getElementById("resultPage").style.display = "block";
   document.getElementById("resultDiv").style.height = document.getElementById("resultDiv").offsetWidth - 6;
+}
+
+function redraw() {
+  document.getElementById("formPage").style.display = "block";
+  document.getElementById("resultPage").style.display = "none";
+  document.getElementById("subjectValue").value = "";
+  document.getElementById("yearValue").value = "";
+  document.getElementById("monthValue").value = "";
+  document.getElementById("dayValue").value = "";
 }
